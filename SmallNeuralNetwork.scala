6d20318db9c461c621deb5428c5c9fe0d5054aee
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2015-11-22 20:26:44 -0500

    Reinitialized weights to make the nn more accurate

--- SmallNeuralNetwork.scala
+++ SmallNeuralNetwork.scala
@@ -1,36 +1,33 @@
 object Run {
   def main(args: Array[String]): Unit = {
+    var errors : Seq[Double] = Seq()
     val e = 2.7182818284590452353602874713527
     val observations : List[List[Int]] = List(List(0,0,1),List(0,1,1),List(1,0,1),List(1,1,1))
     val target = List(0,1,1,0)
-    var hiddenLayerNeurons = for (i <- 0 to 4) yield Seq.fill(3)(scala.util.Random.nextDouble())
-    var outerLayerNeurons = Seq.fill(5)(scala.util.Random.nextDouble())
-    for(epoch <- 0 to 6000){
-      for (observation <- observations){
+    var weightsHiddenNeurons = for (i <- 0 to 4) yield Seq.fill(3)(scala.util.Random.nextDouble() * 2 -1)
+    var weightsOuterLayerNeuron = Seq.fill(5)(scala.util.Random.nextDouble() * 2 -1)
+    for(epoch <- 0 to 60000){
+      for (obsIndex <- 0 to observations.length -1){
         var hiddenLayerOutputs : List[Double] = List()
-        for (neuron <- hiddenLayerNeurons){
-          var dotProduct = (for (i <- 0 to observation.length-1) yield neuron(i) * observation(i)).sum
-          hiddenLayerOutputs = (hiddenLayerOutputs :+ (1/(1+scala.math.pow(e,-1 * dotProduct))))
+        for (neuron <- weightsHiddenNeurons){
+          val dotProduct = (for (i <- 0 to observations(obsIndex).length-1) yield neuron(i) * observations(obsIndex)(i)).sum
+          hiddenLayerOutputs = hiddenLayerOutputs :+ (1/(1+scala.math.pow(e,-1 * dotProduct)))
         }
-        var dotProduct = (for (i <- 0 to hiddenLayerOutputs.size -1) yield outerLayerNeurons(i) * hiddenLayerOutputs(i)).sum
-        val outerLayerOutput =  (1/(1+scala.math.pow(e,-1 * dotProduct)))
-        var error = target(0) - outerLayerOutput
-        println(error) // See how error improves as the epochs progress
-        val deltaOuterLayer = error * ((outerLayerOutput) * (1 - outerLayerOutput))
-        for (i <- 0 to outerLayerNeurons.length -1){
-          outerLayerNeurons = outerLayerNeurons.updated(i, outerLayerNeurons(i) + (deltaOuterLayer * hiddenLayerOutputs(i)))
+        val dotProduct = (for (i <- 0 to hiddenLayerOutputs.size -1) yield weightsOuterLayerNeuron(i) * hiddenLayerOutputs(i)).sum
+        val outerLayerOutput =  1/(1+scala.math.pow(e,-1 * dotProduct))
+        val error = target(obsIndex) - outerLayerOutput
+        errors = errors :+ Math.abs(error)
+        val deltaOuterLayer = error * (outerLayerOutput * (1 - outerLayerOutput))
+        for (i <- 0 to weightsOuterLayerNeuron.length -1){
+          weightsOuterLayerNeuron = weightsOuterLayerNeuron.updated(i, weightsOuterLayerNeuron(i) + (deltaOuterLayer * hiddenLayerOutputs(i)))
         }
-        for (neuronIndex <- 0 to hiddenLayerNeurons.length -1){
+        for (neuronIndex <- 0 to weightsHiddenNeurons.length -1){
           val deltaInnerLayer = deltaOuterLayer * (hiddenLayerOutputs(neuronIndex) * (1- hiddenLayerOutputs(neuronIndex)))
-          var newWeights : Seq[Double] = Seq()
-          for (weightIndex <- 0 to hiddenLayerNeurons(neuronIndex).length -1){
-            val oldVal = hiddenLayerNeurons(neuronIndex)(weightIndex)
-            val newVal = oldVal + deltaInnerLayer * observation(weightIndex)
-            newWeights = (newWeights :+ newVal)
-          }
-          hiddenLayerNeurons = (hiddenLayerNeurons.updated(neuronIndex,newWeights))
+          val newWeights = for (weightIndex <- 0 to weightsHiddenNeurons(neuronIndex).length -1) yield weightsHiddenNeurons(neuronIndex)(weightIndex) + deltaInnerLayer * observations(obsIndex)(weightIndex)
+          weightsHiddenNeurons = (weightsHiddenNeurons.updated(neuronIndex,newWeights))
         }
       }
     }
+    println(errors.sum / errors.length)
   }
 }
